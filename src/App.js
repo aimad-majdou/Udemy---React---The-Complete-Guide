807771d87737dd09c1628ae8c9264dd41711ff54
Aimad MAJDOU <aimad.majdou@gmail.com>
2020-06-01 20:45:15 +0200

    Rendering content dynamically

--- src/App.js
+++ src/App.js
@@ -10,30 +10,37 @@ const App = props => {
       {name: "Byakuya", age:20},
       {name: "Ichigo"},
       {name: "Orihime"}
-    ]
+    ],
+    isDisplayed: false
   });
 
-  const switchName = (newName) => {
-    personsState.persons[0].name = newName;
+  const showNames = () => {
     setPersons({
-      persons: personsState.persons
+      persons: personsState.persons,
+      isDisplayed: !personsState.isDisplayed
     });
   }
 
   const changeAge = (event) => {
     personsState.persons[0].age = event.target.value;
     setPersons({
-      persons: personsState.persons
+      persons: personsState.persons,
+      isDisplayed: personsState.isDisplayed
     });
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <button onClick={switchName.bind(this, "Aimad")}>Switch name</button>
-        <Person name={personsState.persons[0].name} age={personsState.persons[0].age} changed={changeAge}></Person>
-        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>></Person>
-        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>></Person>
+        <button onClick={showNames}>{personsState.isDisplayed ? "Hide names": "Show names"}</button>
+        { personsState.isDisplayed ?
+          <div>
+            <Person name={personsState.persons[0].name} age={personsState.persons[0].age} changed={changeAge}></Person>
+            <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>></Person>
+            <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>></Person>
+          </div>
+          : null
+        }
       </header>
     </div>
   );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react';
 class Person extends Component {
     render() {
         return (
-            <div style={{margin: "20px"}}>
-                <div>I'm {this.props.name}</div>
-                <input type="text" onChange={this.props.changed} value={this.props.age} />
-                {this.props.age ? (<div>My age: {this.props.age}</div>):null}
+            <div>
+                <div style={{margin: "20px"}}>
+                    <div>I'm {this.props.name}</div>
+                    <input type="text" onChange={this.props.changed} value={this.props.age} />
+                    {this.props.age ? (<div>My age: {this.props.age}</div>):null}
+                </div>
             </div>
         );
     }
